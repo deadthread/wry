315a18bf2f468c0e4d9440eb61d444287cf62050
Filippo Milesi <filippo.milesi@cms.it>
2024-10-14 08:55:50 +0200

    fix: exposed cookie values

--- src/cookie.rs
+++ src/cookie.rs
@@ -10,3 +10,41 @@ pub struct Cookie {
     pub(crate) same_site: i32,
     pub(crate) value: String
 }
+
+impl Cookie {
+    pub fn domain(&self) -> &str {
+        &self.domain
+    }
+
+    pub fn expires(&self) -> f64 {
+        self.expires
+    }
+
+    pub fn is_http_only(&self) -> bool {
+        self.is_http_only
+    }
+
+    pub fn is_secure(&self) -> bool {
+        self.is_secure
+    }
+
+    pub fn is_session(&self) -> bool {
+        self.is_session
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn path(&self) -> &str {
+        &self.path
+    }
+
+    pub fn same_site(&self) -> i32 {
+        self.same_site
+    }
+
+    pub fn value(&self) -> &str {
+        &self.value
+    }
+}
